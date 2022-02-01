86a88ad7feb2eaac2c6f43ea0fef49016ededd51
techyminati <sinha.aryan03@gmail.com>
2022-02-01 14:53:46 +0530

    r1: Initial Skeleton Tree
    Change-Id: I571cb013a34e8dd3b0702c59b2a69a4969f9dd93
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

---
+++ cipher_r1.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020-2022 The CipherOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from RMX3242 device
+$(call inherit-product, device/rabbit/r1/device.mk)
+
+# Inherit some common CipherOS stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cipher_r1
+PRODUCT_DEVICE := r1
+PRODUCT_BRAND := rabbit
+PRODUCT_MODEL := rabbit r1
+PRODUCT_MANUFACTURER := rabbit
+
+PRODUCT_GMS_CLIENTID_BASE := android-rabbit
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="r1-user 13 TP1A.220905.016 204 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "rabbit/r1/r1:13/TP1A.220905.016/20240509094513:user/release-keys"
