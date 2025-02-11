bf72968e782fbf2e9dcd96f87bc48b10c694f286
Leo Liou <leoliou@google.com>
2025-02-11 21:31:42 +0530

    r1: Inherit the virtual a/b config For devices launching with virtual A/B, inherit the virtual A/B device base configuration.
    Bug: 146466421
    Bug: 149155051
    Test: boot to home, check system property of virtual_ab
    Test: Run full storage-qa test
    Change-Id: I8ebbbe897dc9278f43e37a6a9599b89d68419b27
    Signed-off-by: Leo Liou <leoliou@google.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- cipher_r1.mk
+++ cipher_r1.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from RMX3242 device
 $(call inherit-product, device/rabbit/r1/device.mk)
