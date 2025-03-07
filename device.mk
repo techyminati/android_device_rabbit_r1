53ec5383caaf46d26d357e1a93a6de6c3cc3de4e
techyminati <techyminati@outlook.com>
2025-03-07 08:45:53 +0000

    r1: Copy some missing xmls from AOSP
    Signed-off-by: techyminati <techyminati@outlook.com>

--- device.mk
+++ device.mk
@@ -114,3 +114,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     StepMotorControls
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
